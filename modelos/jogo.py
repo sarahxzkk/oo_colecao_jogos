0053eb6050e187ccea886019d4ecf9ed95f10d9b
Sarah Gomes <131700078+sarahxzkk@users.noreply.github.com>
2024-06-13 11:24:07 -0300

    jogo

--- modelos/jogo.py
+++ modelos/jogo.py
@@ -1,23 +1,31 @@
+#metodo especial construtor __ini__
+#metodo especial __srt__ para mostar um objeto em forma de texto
+
 class Jogo:
     jogos = []
-
-    def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = True
-        Jogo.jogos.append(self)
-
-    def __str__(self):
-        return f'{self.nome} - {self.categoria}'
     
-    def listar_jogos():
-        for jogo in Jogo.jogos:
-            print(f'{jogo.nome} - {jogo.categoria} - {jogo.ativo}')
-      
-
-jogo_zelda = Jogo('Zelda', 'RPG')
+    def __init__(self, nome, categoria):        
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Jogo.jogos.append(self)
+    def __str__(self):
+        return f'{self._nome} | {self._categoria}'
+    @classmethod
+    def listar_jogos(cls):
+        print(f"{'Nome do jogo'.ljust(20)} | {'Categoria'.ljust(20)} | {'Status'}")
+        for jogo in cls.jogos:
+            print(f'{jogo._nome.ljust(20)} | {jogo._categoria.ljust(20)} | {jogo.ativo}')    
+    @property
+    def ativo(self):
+        return '☑' if self._ativo else '☐'
+    def alterar_status(self):
+        self._ativo = not self._ativo    
+     
+    
+    
+jogo_zelda = Jogo('zelda', 'RPG')
+jogo_zelda.alterar_status()
 jogo_metroid = Jogo('Metroid', 'Plataforma')
-
-jogos = [jogo_zelda, jogo_metroid]
 
 Jogo.listar_jogos()
